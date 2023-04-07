264f3c6cd4aa8830eed4a553b1ad4006e1f97b06
Maksim Mordvintsev <mordvintsevmv@gmail.com>
2023-04-08 01:02:56 +0400

    Refactoring: refactoring Input Component to be more reusable

---
+++ functional/components/Input.js
@@ -0,0 +1,11 @@
+const Input = ({placeholder}) => {
+
+    // Search input
+    const search_field = document.createElement('input')
+    search_field.classList.add('text-input');
+    search_field.placeholder = placeholder
+
+    return search_field
+}
+
+export default Input
