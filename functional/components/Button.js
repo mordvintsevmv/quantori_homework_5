dd68739a2a16fca00ecfbed2f74aad517f8eee55
Maksim Mordvintsev <mordvintsevmv@gmail.com>
2023-04-08 01:00:51 +0400

    Feature: Implementing transparent design for Button Component

--- functional/components/Button.js
+++ functional/components/Button.js
@@ -1,14 +1,20 @@
 /**
  * Button component
  * @param text {string}
+ * @param isTransparent {boolean}
  * @param onClick {function}
  * @returns {HTMLButtonElement} - Button element
  */
-const Button = ({text, onClick}) => {
+const Button = ({text, isTransparent = false, onClick}) => {
+
     const button = document.createElement("button");
     button.innerHTML = text;
     button.onclick = onClick;
     button.classList.add('button')
+
+    if (isTransparent){
+        button.classList.add('button--isTransparent')
+    }
 
     return button;
 }
