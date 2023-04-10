56257f1317f601f99f3113deea280ee0e6a35d18
Maksim Mordvintsev <mordvintsevmv@gmail.com>
2023-04-10 21:53:55 +0400

    Feature: implementing Modal Component

---
+++ oop/components/Modal.js
@@ -0,0 +1,29 @@
+class Modal extends Component {
+    constructor() {
+        super();
+    }
+
+    /**
+     * @override
+     * @param props
+     * @param props.closeModal {function}
+     * @param props.children {HTMLElement}
+     * @returns {HTMLDivElement} - Modal element
+     * */
+    render(props) {
+
+        const modal = document.createElement('div')
+        modal.classList.add('modal')
+        modal.onclick = (event) => {
+            event.stopPropagation()
+        }
+        modal.append(props.children)
+
+        return super.render({
+            onClick: props.closeModal,
+            children: modal,
+            style: this.state.style,
+            className: ['overlay']
+        });
+    }
+}
