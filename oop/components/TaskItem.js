0aedc364b7be0ef094b646fe58dcffcda967bff3
Maksim Mordvintsev <mordvintsevmv@gmail.com>
2023-04-10 22:14:06 +0400

    Feature: implementing TaskItem And TaskList Components

---
+++ oop/components/TaskItem.js
@@ -0,0 +1,120 @@
+const month_array = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+const day_array = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+class TaskItem extends Component {
+    constructor() {
+        super();
+    }
+
+    /**
+     * @override
+     * @param props
+     * @param props.id {number}
+     * @param props.isChecked {boolean}
+     * @param props.title {string}
+     * @param props.tag {string}
+     * @param props.date {string | Date}
+     * @param props.deleteItem {function}
+     * @param props.checkItem {function}
+     * @returns {HTMLDivElement} - HTML code for TaskItem element     */
+    render(props) {
+
+
+        // Parsing date (for situations when there is string instead of Date)
+        const parsed_date = new Date(Date.parse(props.date))
+
+        // Creating text for date
+        const today = new Date();
+
+        let day_text;
+
+        if (parsed_date.getFullYear() === today.getFullYear()
+            && parsed_date.getMonth() === today.getMonth()
+            && parsed_date.getDate() === today.getDate()
+        ) {
+            day_text = 'Today'
+        } else if (parsed_date.getFullYear() === today.getFullYear()
+            && parsed_date.getMonth() === today.getMonth()
+            && parsed_date.getDate() - today.getDate() === 1
+        ) {
+            day_text = 'Tomorrow'
+        } else if (parsed_date.getFullYear() === today.getFullYear()
+            && parsed_date.getMonth() === today.getMonth()
+            && parsed_date.getDate() - today.getDate() === -1
+        ) {
+            day_text = 'Yesterday'
+        } else {
+            day_text = `${day_array[parsed_date.getDay()]}, ${parsed_date.getDate()} ${month_array[parsed_date.getMonth()]}`
+        }
+
+
+        // Checkbox Button
+        const checkbox_button = document.createElement('button')
+        checkbox_button.classList.add('task-item__checkbox')
+
+        const checkbox_img = document.createElement('img')
+        if (props.isChecked) {
+            checkbox_img.src = 'img/checkbox-disabled.svg'
+            checkbox_img.alt = 'Checkbox - uncheck'
+            checkbox_img.classList.add('task-item__checkbox-img--checked')
+        } else {
+            checkbox_img.src = 'img/checkbox-unchecked.svg'
+            checkbox_img.alt = 'Checkbox - check'
+            checkbox_img.classList.add('task-item__checkbox-img--unchecked')
+        }
+
+        checkbox_button.append(checkbox_img)
+        checkbox_button.onclick = () => props.checkItem(props.id)
+
+        // Info wrapper (Title, tag and date)
+        const task_item_info = document.createElement('div')
+        task_item_info.classList.add("task-item__info")
+
+        // Title
+        const task_item_title = document.createElement('h3')
+        task_item_title.classList.add("task-item__title")
+        task_item_title.innerText = props.title
+
+        // Bottom wrapper (Tag and date)
+        const task_item_bottom = document.createElement('div')
+        task_item_bottom.classList.add("task-item__bottom")
+
+        // Tag
+        const task_item_tag = new TaskTag().render({name: props.tag, isColored: !props.isChecked})
+        task_item_tag.classList.add("task-item__tag")
+
+        // Date Text
+        const task_item_date = document.createElement('div')
+        task_item_date.classList.add("task-item__date")
+        task_item_date.innerText = day_text
+
+        // Delete Button
+        const delete_button = document.createElement('button')
+        delete_button.classList.add("task-item__delete")
+        delete_button.onclick = () => props.deleteItem(props.id);
+
+        const delete_button_img = document.createElement('img')
+        delete_button_img.src = 'img/delete-new-value.svg'
+        delete_button_img.alt = 'delete'
+
+
+        // Appending
+        delete_button.append(delete_button_img)
+        task_item_bottom.append(task_item_tag, task_item_date)
+        task_item_info.append(task_item_title, task_item_bottom)
+
+        let children = []
+
+        if (props.isChecked) {
+            children.push(checkbox_button, task_item_info)
+        } else {
+            children.push(checkbox_button, task_item_info, delete_button)
+        }
+
+        return super.render({
+            children: children,
+            style: this.state.style,
+            className: ['task-item']
+        });
+    }
+}
---
+++ oop/components/TaskList.js
@@ -0,0 +1,53 @@
+class TaskList extends Component {
+    constructor() {
+        super();
+    }
+
+    /**
+     * @override
+     * @param props
+     * @param props.title {string}
+     * @param props.items {Object[]}
+     * @param props.deleteItem {function}
+     * @param props.checkItem {function}
+     * @returns {HTMLElement} - List element
+     * */
+    render(props) {
+
+        // Title
+        const list_title = document.createElement('h2')
+        list_title.classList.add('task-list__title')
+        list_title.innerText = props.title
+
+        // Items - array of li
+        const list_items = props.items.map((item) => {
+            const li = document.createElement('li')
+            li.append(new TaskItem().render({
+                id: item.id,
+                isChecked: item.isChecked,
+                title: item.title,
+                tag: item.tag,
+                date: item.date,
+                deleteItem: props.deleteItem,
+                checkItem: props.checkItem
+                }
+            ))
+            return li
+        });
+
+        // Items - ul
+        const ul_wrapper = document.createElement('div')
+        ul_wrapper.classList.add('task-list__items')
+        const ul = document.createElement("ul");
+
+        // Appending
+        ul.append(...list_items);
+        ul_wrapper.append(ul)
+
+        return super.render({
+            children: [list_title, ul_wrapper],
+            className: ['task-list']
+
+        });
+    }
+}
