91bfa23a04019e866d1c9b3fb9fb588196f1dad9
Maksim Mordvintsev <mordvintsevmv@gmail.com>
2023-04-14 17:36:18 +0400

    Fix: fixing bug with last_id on empty array

--- functional/functional.js
+++ functional/functional.js
@@ -19,7 +19,7 @@ const task_items = load_items() || [
 
 let state = {
     items: task_items,
-    last_id: task_items.reduce((max, item) => max > item.id ? max : item.id, task_items[0].id),
+    last_id: task_items.reduce((max, item) => max > item.id ? max : item.id, task_items[0]?.id || 0),
     isModal: false
 }
 
