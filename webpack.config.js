ea1624fc9071573779f0ac201b75edc7cfa78d14
Maksim Mordvintsev <mordvintsevmv@gmail.com>
2023-04-21 00:13:27 +0400

    Feature: configuring Webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: './oop/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: "bundle.js"
+    },
+    mode: "development",
+    devServer: {
+        static: path.resolve(__dirname, "./build"),
+        compress: true,
+        port: 8080,
+        open: true,
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loader: 'file-loader'
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            title: 'Webpack Config',
+            template: './index.html',})
+    ]
+}
